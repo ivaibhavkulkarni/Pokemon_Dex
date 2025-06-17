e6d23fee7386bb2aa1af424ceeb3fd3a45db97d7
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-06-17 23:00:58 +0530

    Add filtering by fav

--- Dex/ContentView.swift
+++ Dex/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @FetchRequest<Pokemon>(sortDescriptors: [SortDescriptor(\.id)], animation: .default) private var pokedex
     
     @State private var searchText = ""
+    @State private var filterByFavorites = false
     
     let fetcher = FetchService()
     
@@ -25,6 +26,10 @@ struct ContentView: View {
             predicates.append(NSPredicate(format: "name contains[c] %@", searchText))
         }
         // Filter by favorite predicate
+        
+        if filterByFavorites {
+            predicates.append(NSPredicate(format: "favorite == %d", true))
+        }
         
         // combine predicate
         return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
@@ -45,8 +50,14 @@ struct ContentView: View {
                         .frame(width: 100, height: 100)
                         
                         VStack(alignment: .leading) {
-                            Text(pokemon.name!.capitalized)
-                                .fontWeight(.bold)
+                            HStack {
+                                Text(pokemon.name!.capitalized)
+                                    .fontWeight(.bold)
+                                if pokemon.favorite {
+                                    Image(systemName: "star.fill")
+                                        .foregroundStyle(.yellow)
+                                }
+                            }
                             HStack {
                                 ForEach(pokemon.types!, id:\.self) { type in
                                     Text(type.capitalized)
@@ -69,12 +80,19 @@ struct ContentView: View {
             .onChange(of: searchText) {
                 pokedex.nsPredicate = dynamicPredicate
             }
+            .onChange(of: filterByFavorites) {
+                pokedex.nsPredicate = dynamicPredicate
+            }
             .navigationDestination(for: Pokemon.self) {pokemon in
                 Text(pokemon.name ?? "no name")
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                    Button{
+                        filterByFavorites.toggle()
+                    }label: {
+                        Label("Filter By Favorites", systemImage: filterByFavorites ? "star.fill" : "star")
+                    }
                 }
                 ToolbarItem {
                     Button("Add Item", systemImage: "plus") {
@@ -101,7 +119,12 @@ struct ContentView: View {
                     pokemon.specialAttack = fetchedPokemon.specialAttack
                     pokemon.specialDefense = fetchedPokemon.specialDefense
                     pokemon.speed = fetchedPokemon.speed
+                    pokemon.sprite = fetchedPokemon.sprite
                     pokemon.shiny = fetchedPokemon.shiny
+                    
+                    if pokemon.id % 2 != 0 {
+                        pokemon.favorite = true
+                    }
                     
                     try viewContext.save()
                 }catch{
